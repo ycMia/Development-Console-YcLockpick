6399a4faaa670f6dbbb11df2c857d24e9f697361
ycMia <zzlyx20123201@126.com>
2020-08-16 17:40:07 +0800

    deleted IDS

--- YcLockpick/YcBitBase.cpp
+++ YcLockpick/YcBitBase.cpp
@@ -3,14 +3,16 @@
 #include "pch.h"
 #include "YcBitBase.h"
 
-inline YcBiter::YcBiter(int length, int id) : _length(length), _classID(id)
+inline YcBiter::YcBiter(int length)
 {
+	_length = length;
 	_bData = new bool[_length];
 	memset(_bData, 0, _length * sizeof(bool));
 }
 
-inline YcBiter::YcBiter(int length, bool * inData, int id) : _length(length), _classID(id)
+inline YcBiter::YcBiter(int length, bool * inData) 
 {
+	_length = length;
 	//Way of Clone
 	_bData = new bool[_length];
 	memset(_bData, 0, _length * sizeof(bool));
@@ -181,25 +183,6 @@ inline string YcBiter::Debug_GetRoughDataString()
 	return dstr;
 }
 
-//inline bool YcBiterComputable::XOR_sameWidth(YcBiter & a,YcBiter & b)
-//{
-//	if (a.Length() == b.Length() &&
-//		_length==a.Length() &&
-//		_length==b.Length() )
-//	{
-//		bool * pb = new bool[a.Length()];
-//		for (int i = 0; i < a.Length(); i++)
-//		{
-//			_bData[i] = a._bData[i] ^ b._bData[i];
-//		}
-//	}
-//	else
-//	{
-//		return false;
-//	}
-//	return true;
-//}
-
 inline bool YcBiterComputable::XOR_sameWidth(bool * a, bool * b)
 {
 	if (_bData == nullptr)	return false;
@@ -211,3 +194,45 @@ inline bool YcBiterComputable::XOR_sameWidth(bool * a, bool * b)
 	delete[] b;
 	return true;
 }
+
+inline YcBiterComputable YcBiterComputable::operator xor(YcBiter & target)
+{
+	if (_length < target.Length())
+	{
+		Widen_EmptySide(target.Length() - _length, true);
+	}
+	YcBiterComputable ybc;
+	if (ybc.XOR_sameWidth(_bData, target.GetBoolArray(target.Length())))
+		return ybc;
+	else
+		return YcBiterComputable();
+}
+
+inline bool * YcBiter :: deleteBuffer_bData(void)
+{
+	if (_bData != nullptr)
+	{
+		delete[] _bData;
+		_bData = NULL;
+	}//多次执行delete(或delete[])将会导致问题出现
+	else
+	{
+		_bData = NULL;
+	}
+	_length = 0;
+	return _bData;
+}
+//返回新的_bData所指向的地址(正常应该是NULL)
+
+inline bool * YcBiter :: strToBool_RS(string str2)
+{
+	bool * pOut = new bool[str2.length()];
+	for (int i = 0; i < (int)str2.length(); i++)
+	{
+		if (str2[i] == '0')
+			pOut[i] = false;
+		else
+			pOut[i] = true;
+	}
+	return pOut;
+}//辅助函数,将形如"1000100100"这类的string转为bool的数组 \n CWS指CoverWrite_S,此函数在非特殊情况下应该只与YcBiter_CoverWrite_RoughString或其他类似函数配合使用,所以我把它放在private中
--- YcLockpick/YcLockpick.cpp
+++ YcLockpick/YcLockpick.cpp
@@ -10,23 +10,8 @@ using namespace std;
 //--程序入口点main()--
 int main()
 {
-	YcBiter *a = new YcBiter();
-	YcBiter *b = new YcBiter();
-	YcBiterComputable *c = new YcBiterComputable();
-	while (true)
-	{
-		a = new YcBiter();
-		b = new YcBiter();
-		a->CoverWrite_RoughString("11 0110 0101 0011 1100 0000 0001 1101 0101 0101");
-		b->CoverWrite_RoughString("11 0110 0000 0011 1100 0000 0001 1101 0101 0101");
-		c = new YcBiterComputable(a->Length());
-
-		if (c->XOR_sameWidth(a->GetBoolArray(a->Length()), b->GetBoolArray(b->Length())))
-			cout << c->Debug_GetRoughDataString() << endl;
-		a->~YcBiter(); a = NULL;
-		b->~YcBiter(); b = NULL;
-		c->~YcBiterComputable(); c = NULL;
-		//system("pause");
-	}
+	YcBiter a("10101110101000111");
+	cout << a.Debug_Get_bData_Hex() << endl;
+	cout << a.Debug_GetRoughDataString() << endl;
 }
 
