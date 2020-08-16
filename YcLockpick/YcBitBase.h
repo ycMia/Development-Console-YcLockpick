70f2409812745be046704a0210eb4f8426abe5f8
ycMia <zzlyx20123201@126.com>
2020-08-17 03:06:42 +0800

    0

--- YcLockpick/YcBitBase.h
+++ YcLockpick/YcBitBase.h
@@ -56,6 +56,8 @@ public:
 	~YcBiterComputable() {}
 
 	//----Construction----
+	YcBiterComputable(YcBiter & yb) : YcBiter(yb.Length(), yb.GetBoolArray(yb.Length()) ) {}
+
 	YcBiterComputable(void) : YcBiter() {}
 
 	YcBiterComputable(int length) : YcBiter(length){}
--- YcLockpick/YcLockpick.cpp
+++ YcLockpick/YcLockpick.cpp
@@ -10,8 +10,14 @@ using namespace std;
 //--程序入口点main()--
 int main()
 {
-	YcBiter a("10101110101000111");
+	YcBiter a("11 1010 1110 0100 0111");
 	cout << a.Debug_Get_bData_Hex() << endl;
 	cout << a.Debug_GetRoughDataString() << endl;
+
+	YcBiterComputable ybc(a);
+	a.~YcBiter();
+
+	cout << ybc.Debug_Get_bData_Hex() << endl;
+	cout << ybc.Debug_GetRoughDataString() << endl;
 }
 
