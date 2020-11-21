edc617f942ff8b916a62380d7b6bfc5e9b394d34
ycMia <zzlyx20123201@126.com>
2020-11-21 13:32:23 +0800

    restart Project

--- YcLockpick/YcBitBase.h
+++ YcLockpick/YcBitBase.h
@@ -1,3 +1,4 @@
+//--YcBitBase.h--
 #pragma once
 
 #include<iostream>
@@ -5,74 +6,50 @@
 
 using namespace std;
 
-class YcBiter
+namespace yctd
 {
-public:
-	~YcBiter()
+	class CDataArea//连续的数据存放点
 	{
-		deleteBuffer_bData();
-	}
+	public:
+		CDataArea(void)
+		{
+			vSetArea(0);
+		}
 
-	//----Constructions----
-	YcBiter(void)
+		void vSetArea(int nLength)
+		{
+			m_nLength = nLength;
+			m_pchData = new char[m_nLength];
+		}
+
+		int nGetLength(void)
+		{
+			return m_nLength;
+		}
+	protected:
+		int m_nLength;
+		char * m_pchData;
+	};
+
+	class CQueue : public CDataArea
 	{
-		_length = 1;
-		_bData = new bool[1];
-		*_bData = false;
-	}
-	//Default Length for 1
-	YcBiter(int length);
-	YcBiter(int length, bool * inData);//用以克隆
-	YcBiter(string str) {
-		CoverWrite_RoughString(str);
-	}
+	public:
+		CQueue(void)
+		{
+			
+		}
+	private:
+		int m_nCurrent;
+	};
 
-	//----UseageArea
-	bool Widen_EmptySide(int n, bool true2left_OR_false2right);//mode == true时拓宽左边(值不变),mode == false时拓宽右边(低位向高位移动),成功时返回tre
-	bool Flush_bData(int n);	//成功时返回true
-	void CoverWrite(bool * data, int inLength);
-	void CoverWrite_RoughString(string str);
-	int Length() { return _length; }
-	
-	//----DEBUG&ShowArea
-	bool dump8BitArray(int nBytes,unsigned char address[]);//成功时返回true, address的delete需要手动操作
-	bool * GetBoolArray(int nbitslength);
-	int Debug_GetLen() { return _length; }
-	string Debug_GetRoughDataString();//返回形如"10001010"的字符串,而不是raw
-	string Debug_Get_bData_Hex(bool format = true);
-	
-protected:
-	bool * deleteBuffer_bData(void);
-	bool * strToBool_RS(string str2);
-	
-	bool * _bData = NULL;
-	int _length = 0;
 
-};
 
-class YcBiterComputable : public YcBiter
-{
-public:
-	~YcBiterComputable() {}
+	class IYcBase
+	{
+	public:
+		virtual int ngetValue() = 0;
+	private:
+		
+	};
 
-	//----Construction----
-	YcBiterComputable(YcBiterComputable & yb) 
-		: YcBiter(yb.Length(), yb.GetBoolArray(yb.Length())) 
-	{}
-	YcBiterComputable(YcBiter & yb) 
-		: YcBiter(yb.Length(), yb.GetBoolArray(yb.Length()) ) 
-	{}
-
-	YcBiterComputable(void) : YcBiter() {}
-
-	YcBiterComputable(int length) : YcBiter(length){}
-	YcBiterComputable(int length,bool * inData) : YcBiter(length,inData) {}
-
-	YcBiterComputable operator^ (const YcBiterComputable & target);
-	void operator= (const YcBiterComputable & target);
-	//运算符重载参考https://www.cnblogs.com/linuxAndMcu/p/10395383.html
-	//重载中const的细则见https://blog.csdn.net/weixin_43734095/article/details/104465307
-
-private:
-	
-};
+}
--- YcLockpick/YcLockpick.cpp
+++ YcLockpick/YcLockpick.cpp
@@ -3,42 +3,16 @@
 
 #define DEBUG
 
-#include "YcBitBase.cpp"
+#include "YcBitBase.h"
 
 using namespace std;
 
-//--程序入口点main()--
 int main()
 {
-	YcBiter a("11 1010 1110 0100 0111");
-	YcBiter b("1111 1111 1111 1111 1111");
-	cout << "a" << endl;
-	cout << a.Debug_Get_bData_Hex() << endl;
-	cout << a.Debug_GetRoughDataString() << endl;
-	a.Widen_EmptySide(7, true);
-
-	YcBiterComputable ybc(a);
-	YcBiterComputable ybc2(b);
-	a.~YcBiter();
-	b.~YcBiter();
-	cout << "ybc" << endl;
-	cout << ybc.Debug_Get_bData_Hex() << endl;
-	cout << ybc.Debug_GetRoughDataString() << endl;
-
-	ybc = ybc ^ ybc2;
-	cout << "ybc2" << endl;
-	cout << ybc2.Debug_Get_bData_Hex() << endl;
-	cout << ybc2.Debug_GetRoughDataString() << endl;
-	cout << "ybc" << endl;
-	cout << ybc.Debug_Get_bData_Hex() << endl;
-	cout << ybc.Debug_GetRoughDataString() << endl;
-
-	ybc = ybc ^ ybc2;
-	cout << "ybc2" << endl;
-	cout << ybc2.Debug_Get_bData_Hex() << endl;
-	cout << ybc2.Debug_GetRoughDataString() << endl;
-	cout << "ybc" << endl;
-	cout << ybc.Debug_Get_bData_Hex() << endl;
-	cout << ybc.Debug_GetRoughDataString() << endl;
+	yctd::CQueue queueT;
+	yctd::CQueue queue2T;
+	queue2T.vSetArea(30);
+	cout << queueT.nGetLength() << endl;
+	cout << queue2T.nGetLength() << endl;
 }
 
